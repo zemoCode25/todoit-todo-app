4ebac351e07eab11afbcb37beff0cf9584c16376
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-01-31 21:48:06 +0800

    Added open and close events in the input modal

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,7 +8,7 @@ export function Todo() {
 
   return (
     <main className="w-dvw h-dvh flex justify-center my-16">
-      <div className="flex flex-col w-2/3">
+      <div className="relative flex flex-col w-2/3">
         <div>
           <h1 className="text-4xl font-semibold">todoit tasks!</h1>
           <p className="text-gray-600">
@@ -21,14 +21,17 @@ export function Todo() {
             placeholder="Filter task..."
             type="text"
           />
-          <button className="bg-black px-4 py-1.5 rounded-md text-white cursor-pointer hover:shadow-md">
+          <button
+            onClick={() => setOpenModal(true)}
+            className="bg-black px-4 py-1.5 rounded-md text-white cursor-pointer hover:shadow-md"
+          >
             <span className="px-1 text-white font-bold">+</span>Add task
           </button>
         </div>
         <TaskTable />
         <TaskInputModal
           isOpen={isOpenModal}
-          onClose={() => setOpenModal(close)}
+          onClose={() => setOpenModal(false)}
         />
       </div>
     </main>
--- src/components/todo/TaskInputModal.jsx
+++ src/components/todo/TaskInputModal.jsx
@@ -1,8 +1,34 @@
-export function TaskInputModal() {
+import PropTypes from "prop-types";
+import { useRef, useEffect } from "react";
+
+export function TaskInputModal({ isOpen, onClose }) {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    function handleOutsideClick(e) {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        onClose();
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.addEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
   return (
-    <div className="flex flex-col p-5 bg-white shadow-xl w-[50%] z-20 rounded-lg gap-">
+    <div
+      ref={modalRef}
+      className="absolute left-1/2 top-10 -translate-x-1/2 flex flex-col p-5 bg-white shadow-[0_0_20px_3px_rgba(0,0,0,0.2)] w-[50%] z-20 rounded-lg "
+    >
       <input
-        className="text-lg outline-none py-1 px-2 rounded-md"
+        className="text-lg font-semibold outline-none py-1 px-2 rounded-md"
         type="text"
         placeholder="Task name"
       />
@@ -26,7 +52,6 @@ export function TaskInputModal() {
           className="block px-2 py-1 w-fit text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm appearance-none text-sm cursor-pointer  outline-none focus:ring-0"
           name="cars"
           id="cars"
-          value={"pangit"}
         >
           <option value="volvo">Priority 1st</option>
           <option value="saab">Priority 2nd</option>
@@ -44,8 +69,11 @@ export function TaskInputModal() {
           <option value="audi">Priority 4th</option>
         </select>
       </div>
-      <div className="flex gap-3 ml-auto">
-        <button className="px-4 py-1 bg-gray-100 rounded-md cursor-pointer">
+      <div className="flex gap-3 ml-auto mt-2">
+        <button
+          onClick={onClose}
+          className="px-4 py-1 bg-gray-100 rounded-md cursor-pointer"
+        >
           Cancel
         </button>
         <button className="px-4 py-1 text-white bg-black rounded-md cursor-pointer">
@@ -55,3 +83,8 @@ export function TaskInputModal() {
     </div>
   );
 }
+
+TaskInputModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
