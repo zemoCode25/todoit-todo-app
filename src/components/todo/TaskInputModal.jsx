4464f84a5f7dcc80cfc487b3efd2117473dd52b1
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-02-03 16:30:01 +0800

    Added Feature: Prevent user to send blank inputs

--- src/components/todo/TaskInputModal.jsx
+++ src/components/todo/TaskInputModal.jsx
@@ -6,9 +6,9 @@ export function TaskInputModal({ isOpen, onClose, onTaskSubmit }) {
   const [taskData, setTaskData] = useState({
     taskName: "",
     description: "",
-    priority: "",
-    taskStatus: "",
-    type: "",
+    priority: "Priority 1",
+    taskStatus: "Todo",
+    type: "Personal",
   });
 
   function handleFormChange(e) {
@@ -18,8 +18,10 @@ export function TaskInputModal({ isOpen, onClose, onTaskSubmit }) {
     });
   }
 
-  function handleTaskSubmit() {
+  function handleTaskSubmit(e) {
+    e.preventDefault();
     onTaskSubmit((prevTaskList) => [...prevTaskList, taskData]);
+    onClose();
   }
 
   useEffect(() => {
@@ -45,75 +47,76 @@ export function TaskInputModal({ isOpen, onClose, onTaskSubmit }) {
       ref={modalRef}
       className="absolute top-10 left-1/2 z-20 flex w-[50%] -translate-x-1/2 flex-col rounded-lg bg-white p-5 shadow-[0_0_20px_3px_rgba(0,0,0,0.2)]"
     >
-      <input
-        className="rounded-md px-2 py-1 text-lg font-semibold outline-none"
-        type="text"
-        placeholder="Task name"
-        name="taskName"
-        onChange={handleFormChange}
-      />
-      <textarea
-        name="description"
-        placeholder="description"
-        className="rounded-md px-2 py-1 outline-none"
-        onChange={handleFormChange}
-      ></textarea>
-      <div className="flex gap-3 border-b border-b-gray-200 pb-3">
-        <select
-          className="ml-2 block w-fit cursor-pointer appearance-none rounded-md border border-gray-300 bg-white px-2 py-1 text-sm text-gray-700 shadow-sm outline-none focus:ring-0"
-          name="priority"
-          id="priority"
-          value={taskData.priority}
+      <form className="flex flex-col" action="" onSubmit={handleTaskSubmit}>
+        <input
+          className="rounded-md px-2 py-1 text-lg font-semibold outline-none"
+          type="text"
+          placeholder="Task name"
+          name="taskName"
           onChange={handleFormChange}
-        >
-          <option value="Priority 1st">Priority 1st</option>
-          <option value="Priority 2nd">Priority 2nd</option>
-          <option value="Priority 3rd">Priority 3rd</option>
-          <option value="Priority 4th">Priority 4th</option>
-        </select>
-        <select
-          className="block w-fit cursor-pointer appearance-none rounded-md border border-gray-300 bg-white px-2 py-1 text-sm text-gray-700 shadow-sm outline-none focus:ring-0"
-          name="taskStatus"
-          id="status"
-          value={taskData.taskStatus}
+          required
+        />
+        <textarea
+          name="description"
+          placeholder="description"
+          className="rounded-md px-2 py-1 outline-none"
           onChange={handleFormChange}
-        >
-          <option value="Todo">Todo</option>
-          <option value="In progress">In progress</option>
-          <option value="Backlog">Backlog</option>
-          <option value="Canceled">Canceled</option>
-        </select>
-        <select
-          className="block w-fit cursor-pointer appearance-none rounded-md border border-gray-300 bg-white px-2 py-1 text-sm text-gray-700 shadow-sm outline-none focus:ring-0"
-          name="type"
-          id="type"
-          value={taskData.type}
-          onChange={handleFormChange}
-        >
-          <option value="Personal">Personal</option>
-          <option value="Professional">Professional</option>
-          <option value="Academic">Academic</option>
-          <option value="Health">Health</option>
-          <option value="Hobby">Hobby</option>
-        </select>
-      </div>
-      <div className="mt-2 ml-auto flex gap-3">
-        <button
-          onClick={onClose}
-          className="cursor-pointer rounded-md bg-gray-100 px-4 py-1"
-        >
-          Cancel
-        </button>
-        <button
-          onClick={() => {
-            handleTaskSubmit();
-            onClose();
-          }}
-          className="cursor-pointer rounded-md bg-black px-4 py-1 text-white"
-        >
-          Add Task
-        </button>
-      </div>
+          required
+        ></textarea>
+        <div className="flex gap-3 border-b border-b-gray-200 pb-3">
+          <select
+            className="ml-2 block w-fit cursor-pointer appearance-none rounded-md border border-gray-300 bg-white px-2 py-1 text-sm text-gray-700 shadow-sm outline-none focus:ring-0"
+            name="priority"
+            id="priority"
+            value={taskData.priority}
+            onChange={handleFormChange}
+          >
+            <option value="Priority 1st">Priority 1st</option>
+            <option value="Priority 2nd">Priority 2nd</option>
+            <option value="Priority 3rd">Priority 3rd</option>
+            <option value="Priority 4th">Priority 4th</option>
+          </select>
+          <select
+            className="block w-fit cursor-pointer appearance-none rounded-md border border-gray-300 bg-white px-2 py-1 text-sm text-gray-700 shadow-sm outline-none focus:ring-0"
+            name="taskStatus"
+            id="status"
+            value={taskData.taskStatus}
+            onChange={handleFormChange}
+          >
+            <option value="Todo">Todo</option>
+            <option value="In progress">In progress</option>
+            <option value="Backlog">Backlog</option>
+            <option value="Canceled">Canceled</option>
+          </select>
+          <select
+            className="block w-fit cursor-pointer appearance-none rounded-md border border-gray-300 bg-white px-2 py-1 text-sm text-gray-700 shadow-sm outline-none focus:ring-0"
+            name="type"
+            id="type"
+            value={taskData.type}
+            onChange={handleFormChange}
+          >
+            <option value="Personal">Personal</option>
+            <option value="Professional">Professional</option>
+            <option value="Academic">Academic</option>
+            <option value="Health">Health</option>
+            <option value="Hobby">Hobby</option>
+          </select>
+        </div>
+        <div className="mt-2 ml-auto flex gap-3">
+          <button
+            onClick={onClose}
+            className="cursor-pointer rounded-md bg-gray-100 px-4 py-1"
+          >
+            Cancel
+          </button>
+          <button
+            className="cursor-pointer rounded-md bg-black px-4 py-1 text-white"
+            type="submit"
+          >
+            Add Task
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
